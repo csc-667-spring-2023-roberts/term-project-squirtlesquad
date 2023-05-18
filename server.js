42a3fec99eb928af487a3e783fe4cc9cf07e3f22
shezpc <shezpc@gmail.com>
2023-05-18 03:33:51 -0700

    added live reload statements

--- server.js
+++ server.js
@@ -14,6 +14,21 @@ app.set("views", path.join(__dirname, "backend", "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "backend", "static")));
 
+if (process.env.NODE_ENV === "development") {
+  const livereload = require("livereload");
+  const connectLiveReload = require("connect-livereload");
+
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "backend", "static"));
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+
+  app.use(connectLiveReload());
+}
+
 const rootRoutes = require("./backend/routes/root");
 app.use("/", rootRoutes);
 
