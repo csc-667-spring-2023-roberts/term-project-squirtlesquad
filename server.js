a9d3e91331265063dbbd6061f33f8a49782593e3
shezpc <shezpc@gmail.com>
2023-05-22 04:45:37 -0700

    redid web skeleton, use npm install after pulling before running

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require("path");
 const createError = require("http-errors");
 const cookieParser = require("cookie-parser");
@@ -5,13 +6,19 @@ const morgan = require("morgan");
 
 const express = require("express");
 const app = express();
+const rootRoutes = require("./backend/routes/root");
+
+/*Test, next 3 lines
+const testRoutes = require("./routes/static/test.js");
+
+app.use("/test", testRoutes);*/
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.set("views", path.join(__dirname, "backend", "views"));
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "backend", "static")));
 
 if (process.env.NODE_ENV === "development") {
@@ -29,10 +36,16 @@ if (process.env.NODE_ENV === "development") {
   app.use(connectLiveReload());
 }
 
-const rootRoutes = require("./backend/routes/root");
 app.use("/", rootRoutes);
 
 const PORT = process.env.PORT || 3000;
+/*
+app.use("/", homeRoutes);
+app.use("/games", gamesRoutes);
+app.use("/lobby", lobbyRoutes);
+app.use("/authentication", authenticationRoutes);
+//app.use("/static/test", testRoutes);
+*/
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
