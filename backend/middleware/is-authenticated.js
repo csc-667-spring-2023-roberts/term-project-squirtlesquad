7fadaea99395be67d570e676fffa805025d17869
shezpc <shezpc@gmail.com>
2023-05-25 05:42:15 -0700

    chat is running

--- backend/middleware/is-authenticated.js
+++ backend/middleware/is-authenticated.js
@@ -1,6 +1,5 @@
 const isAuthenticated = (request, response, next) => {
   const { user } = request.session;
-  console.log("User:", user); // Add this line for debugging
   if (user !== undefined && user.id !== undefined) {
     next();
   } else {
---
+++ backend/sockets/constants.js
@@ -0,0 +1,4 @@
+const CHAT_MESSAGE_RECEIVED = "chat-message-received";
+module.exports = {
+  CHAT_MESSAGE_RECEIVED,
+};
